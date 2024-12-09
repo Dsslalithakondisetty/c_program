bcf7a9f89a51dafe1764c16f460ea6d7111a43b7
Dsslalithakondisetty <kondisettidenisri@gmail.com>
2024-12-09 14:57:40 +0545

    Create difference.c

---
+++ difference.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() 
+{
+    int i,n,sumeven=0,sumodd=0;
+    printf("Enter the n value:");
+    scanf("%d",&n);
+    for(i=1;i<=n;++i)
+    {
+        if(i%2==0)
+        {
+        sumeven=sumeven+i;
+    }
+    else
+    {
+        sumodd=sumodd+i;
+    }
+    }
+        printf("The difference is %d\t",sumeven-sumodd);
+    return 0;
+}
