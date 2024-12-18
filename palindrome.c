1507ac4bdfc50d09f1ce82035a68798cf4439c95
Dsslalithakondisetty <kondisettidenisri@gmail.com>
2024-12-18 15:43:42 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,rev=0,rem,temp;
+    printf("Enter the n value:");
+    scanf("%d",&n);
+    temp=n;
+    while(n>0)
+    {
+        rem=n%10;
+        rev=rev*10+rem;
+        n=n/10;
+    }
+    printf("The temp value is %d\n",temp);
+    if(temp==rev)
+    {
+        printf("The number is palindrome");
+    }
+    else
+    {
+        printf("The number is not palindrome");
+    }
+    return 0;
+}
